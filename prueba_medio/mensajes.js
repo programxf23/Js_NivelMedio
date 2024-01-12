b190275e3fd72d2106cd5bcc2341e15e94f08c78
Fernando <fh1273509@gmail.com>
2024-01-12 19:22:30 -0300

    mensajes con prompt, alert, confirm, print

---
+++ prueba_medio/mensajes.js
@@ -0,0 +1,14 @@
+//alert, print, prompt, confirm
+
+window.alert("fer");
+
+//print me manda al cuadro de dialogo para imprimir
+print();
+
+//promtp nos abre la caja de dialogo para colocar un dato
+
+prompt("ingrese un dato");
+
+//confirm nos pregunta si deseamos confirmar algo
+
+confirm("deseas confirmar");
