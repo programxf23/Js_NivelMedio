92748a2ce771d8bfcdc8fe4f5d5f1bee70c86085
Fernando <fh1273509@gmail.com>
2024-01-16 21:39:55 -0300

    stop propagation

---
+++ prueba_medio/stopevent.js
@@ -0,0 +1,19 @@
+/*el event flow nos controla el orden de flujo de los eventos, estan los event bubbling y los capture, 
+el que viene por defecto es el bubbling.
+*/
+
+const button = document.querySelector('.button');
+const contenedor = document.querySelector('.contenedor');
+
+
+
+button.addEventListener('click', (e) => {
+    alert("di click en el buton");
+},true);
+
+contenedor.addEventListener('click', (e) => {
+    alert("di click en el contenedor");
+    e.stopPropagation();
+});
+ 
+//true nos permite priorizar que evento se ejecutara primero
