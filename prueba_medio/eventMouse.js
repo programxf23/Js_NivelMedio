e2d9cf55a2ec27e2de54d5c49df005c9227596fa
Fernando <fh1273509@gmail.com>
2024-01-17 21:37:05 -0300

    eventos mouse

---
+++ prueba_medio/eventMouse.js
@@ -0,0 +1,33 @@
+//eventos del mouse
+
+const button = document.querySelector('.button');
+const contenedor = document.querySelector('.contenedor');
+
+
+/*
+button.addEventListener('dblclick', (e) => {
+    alert("di click en el buton");
+});
+
+*/
+/*
+button.addEventListener("mouseover", (e) => {
+    alert("di click en el buton");
+});
+*/
+
+/*
+button.addEventListener("mouseout", (e) => {
+    alert("di click en el buton");
+});
+*/
+
+/*
+button.addEventListener('contextmenu', (e) => {
+    alert("di click en el buton");
+});
+*/
+
+button.addEventListener('mouseup', (e) => {
+    alert("di click en el buton");
+});
