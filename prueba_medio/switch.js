0d30e2de74858fbf36f986f34777c40b77f093f4
Fernando <fh1273509@gmail.com>
2024-01-22 20:12:45 -0300

    switch

---
+++ prueba_medio/switch.js
@@ -0,0 +1,16 @@
+let expresion = "2";
+
+switch (expresion) {
+  case "1":
+    console.log("esto es una banana");
+    break;
+  case "2":
+    console.log("esto es una manzana");
+    break;
+  case "3":
+    console.log("esto es una pera");
+    break;
+  case "4":
+    console.log("esto es un kiwi");
+    break;
+}
