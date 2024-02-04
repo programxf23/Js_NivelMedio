1dd655f0c54c2405089c33db837612b064a93967
Fernando <fh1273509@gmail.com>
2024-02-04 19:32:12 -0300

    instalando wammp

---
+++ prueba_medio/ayax.js
@@ -0,0 +1,15 @@
+/*ajax
+introduccion e instalacion del servidor
+objeto xmlhttprequest 
+enviar peticiones get
+trabajar el resultado de las peticiones
+objeto activeobject(messirve)
+nueva forma de trabajar el resultado
+enviar peticiones post
+objeto formdata
+*/
+
+//para trabajar con ajax tenemos que tener un servidor
+//ajax es javascript asincrono
+
+//instalacion de wammp
