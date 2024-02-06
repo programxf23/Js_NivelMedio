b946cadd151c2dcf6ede114a4bc1e5967558445e
Fernando <fh1273509@gmail.com>
2024-02-06 18:17:42 -0300

    ajax manejo de peticion anterior

--- prueba_medio/ayax.js
+++ prueba_medio/ayax.js
@@ -13,3 +13,18 @@ objeto formdata
 //ajax es javascript asincrono
 
 //instalacion de wammp
+
+const peticion = new XMLHttpRequest(); //podemos enviar peticiones GET Y POST
+
+peticion.addEventListener("readystatechange", ()=>{
+
+    if(peticion.readystate == 4 && peticion.status == 200){
+        console.log(peticion.status);
+    }
+})
+
+peticion.open("GET", "/info.txt");
+peticion.send();
+
+//si la peticion es 4 y el estatus es 200 esta todo perfecto
+// y de la forma anterior se trabajaba antes.
